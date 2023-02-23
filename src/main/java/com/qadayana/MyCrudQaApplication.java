d7f71f02b0899a9dc77fb3108b812ffb4eac3b22
“Cyber-DIVA” <ivanova.d.g.16@gmail.com>
2023-02-23 19:39:05 +0000

    first commit

---
+++ src/main/java/com/qadayana/MyCrudQaApplication.java
@@ -0,0 +1,13 @@
+package com.qadayana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrudQaApplication.class, args);
+	}
+
+}
